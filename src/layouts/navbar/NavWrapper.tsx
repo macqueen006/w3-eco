75ac446455779498bf18d90051ed35e5b1b28572
macqueen006 <ogbrukomogodwin@gmail.com>
2025-10-17 06:02:54 +0100

    minor modificaton to the navigation

--- src/layouts/navbar/NavWrapper.tsx
+++ src/layouts/navbar/NavWrapper.tsx
@@ -3,7 +3,7 @@ import {cn} from "../../lib/utils.ts";
 
 export const NavWrapper = ({children, className}: { children?: ReactNode, className?: string }) => {
     return (
-        <header className={cn("h-auto border-b border-border backdrop-blur-[20px] bg-background/50 sticky top-0 flex items-center justify-center z-[200]", className)}>
+        <header className={cn("h-auto py-1.5 border-b border-border backdrop-blur-[20px] bg-background/50 sticky top-0 flex items-center justify-center z-[200]", className)}>
             <div className="w-full mx-auto relative px-6 z-[3]">
                 <div className="flex items-center justify-between">
                     {children}
